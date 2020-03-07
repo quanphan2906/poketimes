54f911144a8964ef5f4b4ef46daf448d32b155fb
quanphan2906 <hoangquanphan2906@gmail.com>
2020-03-07 15:25:05 +0700

    Integrate Redux to the project

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,11 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import Pokeball from "../pokeball.png";
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import Pokeball from "../pokeball.png"
+import { connect } from "react-redux"
 
 class Home extends Component {
-    state = {
-        posts: []
-    }
-    componentDidMount(){
-        axios.get("https://jsonplaceholder.typicode.com/posts")
-            .then(res => {
-                this.setState({
-                    posts: res.data.slice(0, 10)
-                })
-            })
-    }
     render(){    
-        const { posts } = this.state;
+        const { posts } = this.props;
         const postList = posts.length ? (
             posts.map(post => {
                 return (
@@ -43,4 +32,13 @@ class Home extends Component {
     }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Home)
+//in the past: HOC(component)
+//but since connect is a function that returns a HOC
+//write connect() => HOC, so writing connect()(Home) is like HOC(Home)
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,25 +1,22 @@
 import React, {Component} from "react"
-import axios from "axios"
+import { connect } from "react-redux"
 
 class Post extends Component {
-    state = {
-        post: null
-    }
-    componentDidMount = () => {
-        let id = this.props.match.params.post_id;
-        axios.get("https://jsonplaceholder.typicode.com/posts/" + id)
-        .then(res => {
-            this.setState({
-                post: res.data
-            })
-        })
+    handleClick = () => {
+        this.props.deletePost(this.props.post.id);
+        this.props.history.push("/");
     }
     render = () => {
-        const { post } = this.state;
+        const { post } = this.props;
         const postEle = post ? (
             <div className="post">
                 <h4 className="center">{post.title}</h4>
                 <p>{post.body}</p>
+                <div className="center">
+                    <button className="btn grey" onClick={this.handleClick}>
+                        Delete Post
+                    </button>
+                </div>
             </div>
         ) : (
             <div className="center">Loading post...</div>
@@ -32,4 +29,22 @@ class Post extends Component {
     }
 }
 
-export default Post
+const mapStateToProps = (state, ownProps) => {
+    let id = ownProps.match.params.post_id;
+    return {
+        post: state.posts.find(post => post.id === id)
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: id => {
+            dispatch({
+                type: "DELETE_POST",
+                id: id
+            })
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
